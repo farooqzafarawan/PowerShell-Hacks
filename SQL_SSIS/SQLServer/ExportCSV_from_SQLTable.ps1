b48d7e2d1637d43ce1a5734dfce14fccc2eaecb5
Farooq Zafar <53262137+farooqzafarawan@users.noreply.github.com>
2020-01-23 18:50:26 +0500

    Update ExportCSV_from_SQLTable.ps1

--- SQL_SSIS/SQLServer/ExportCSV_from_SQLTable.ps1
+++ SQL_SSIS/SQLServer/ExportCSV_from_SQLTable.ps1
@@ -1,9 +1,21 @@
 $instanceName = "localhost"
 $databaseName = "SampleDB"
+$usr = "guest"
+$pwd = "guest"
 $filename = "C:\DATA\Customers.txt"
 $delimiter = "|"
 
 $ExportQry = "SELECT * FROM SampleText" 
 
-Invoke-SqlCmd -Query $ExportQry -ServerInstance $instanceName -Database $databaseName |
-Export-Csv -Delimiter $delimiter -NoType $fileName
+write-host "Start Export" (get-date -Format "HH:mm:ss")
+
+$sqlcmd = Invoke-SqlCmd -Query $ExportQry -ServerInstance $instanceName -Database $databaseName -Username $usr -Password $pwd  
+
+$sqlcmd | Export-Csv -UseQuotes Never -Delimiter $delimiter  -Path $fileName -Encoding UTF8
+
+write-host "Completed:" (get-date -Format "HH:mm:ss")
+
+
+
+
+
