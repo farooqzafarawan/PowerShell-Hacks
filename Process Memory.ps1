dc5bbc638bf60ce3a1d9fb7157f8e1fe6a1d9534
Farooq Zafar <53262137+farooqzafarawan@users.noreply.github.com>
2023-08-02 03:16:03 +0500

    Update Process Memory.ps1

--- Process Memory.ps1
+++ Process Memory.ps1
@@ -1,2 +1,7 @@
 # To get the amount of memory per process used on a 64 bit windows operating system
-Get-Process | Sort-Object WorkingSet64 | Select-Object Name,@{Name='WorkingSet';Expression={($_.WorkingSet64/1KB)}}
+Get-Process| Sort-Object WorkingSet64| Select-Object Name,@{Name='WorkingSet';Expression={($_.WorkingSet64/1KB)}}
+
+
+# Get-Process with total memory usage
+get-process -computername $tag1 | Group-Object -Property ProcessName | 
+    Format-Table Name, @{n='Mem (KB)';e={'{0:N0}' -f (($_.Group|Measure-Object WorkingSet64 -Sum).Sum / 1KB)};a='right'} -AutoSize
